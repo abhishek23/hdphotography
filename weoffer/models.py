9565dae44de08d806ac2a8b08f7c0abdcba19409
Abhishek <abhishek.dalvi231295@gmail.com>
2018-08-10 01:26:23 +0530

    added changes in Event Detail page, Need some changes in Div placement in Event Detail page

--- weoffer/models.py
+++ weoffer/models.py
@@ -4,6 +4,9 @@ class Event(models.Model):
 	name = models.CharField(max_length=20)
 	thumbnail = models.ImageField(upload_to='event_thumbnails')
 
+	def __str__(self):
+		return self.name
+
 class EventImage(models.Model):
 	image = models.ImageField(upload_to='event_images')
 	event_id = models.ForeignKey(Event, related_name='images', on_delete=models.CASCADE)
