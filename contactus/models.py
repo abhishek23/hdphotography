8895a721024c7cf69d45085c76010f9d325b81ec
Abhishek <abhishek.dalvi231295@gmail.com>
2018-09-05 10:41:51 +0530

    Added changes in Contact Us view

--- contactus/models.py
+++ contactus/models.py
@@ -3,5 +3,5 @@ from django.db import models
 class ContactUs(models.Model):
 	name = models.CharField(max_length=100)
 	contactNo = models.CharField(max_length=20)
-	emailId = models.CharField(max_length=254, blank=True)
+	emailId = models.EmailField(max_length=254, blank=True)
 	query = models.CharField(max_length=1000)
