428f8025372fb99d2c4dd16cbff2391c3ddc60aa
Abhishek <abhishek.dalvi231295@gmail.com>
2018-09-11 09:49:54 +0530

    Changes for MyDetails

--- contactus/admin.py
+++ contactus/admin.py
@@ -1,8 +1,9 @@
 from django.contrib import admin
-from .models import ContactUs
+from .models import ContactUs, MyDetails
 
 class ContactUsAdmin(admin.ModelAdmin):
 	list_display = ('name', 'is_done')
 	ordering = ('is_done',)
 
 admin.site.register(ContactUs, ContactUsAdmin)
+admin.site.register(MyDetails)
