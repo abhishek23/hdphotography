309a91b07e2d0addcd440cf9e7cf2cf3c0f84dee
Abhishek <abhishek.dalvi@bistasolutions.com>
2018-09-17 20:22:07 +0530

    Added Weoffer Slider

--- weoffer/urls.py
+++ weoffer/urls.py
@@ -3,6 +3,7 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name='weoffer'),
+    path('slider/', views.slider, name='weoffer_slider'),
     path('events/<int:eventId>', views.eventDetail, name='event_detail'),
     path('events/stories/<int:featuredId>', views.stories, name='featured_stories'),
 ]
--- weoffer/views.py
+++ weoffer/views.py
@@ -11,6 +11,10 @@ def index(request):
 	print(eventSetOfThree)
 	return render(request, 'weoffer/weoffer.html', {'eventRows': eventSetOfThree})
 
+def slider(request):
+	events = models.Event.objects.all().order_by('sequence')
+	return render(request, 'weoffer/weoffer_slider.html', {'events': events})
+
 def eventDetail(request, eventId):
 	event = models.Event.objects.prefetch_related('images').get(pk=eventId)
 	return render(request, 'weoffer/event_detail.html', {'event': event})
